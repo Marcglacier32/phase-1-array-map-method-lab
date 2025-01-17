5989ec187dc7d9effa8fe70c38c05a88fef895a7
Marcglacier32 <mark.thiongo@student.moringaschool.com>
2025-01-16 19:59:15 -0800

    Implemented titleCased function for tutorial titles

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((title) => {
+    return title
+      .split(' ') // Split title into an array of words
+      .map((word) => word[0].toUpperCase() + word.slice(1)) // Capitalize each word
+      .join(' '); // Join words back into a single string
+  });
+};
+
+console.log(titleCased());
